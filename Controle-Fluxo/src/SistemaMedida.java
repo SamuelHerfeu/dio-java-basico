4777df6f8eec41e176450c615ecbea5de91b30a3
samuelherfeu <samuel75rms@gmail.com>
2025-03-18 11:30:11 -0300

    Modo condicional switch/case

---
+++ Controle-Fluxo/src/SistemaMedida.java
@@ -0,0 +1,36 @@
+// Modo condicional switch / case
+
+public class SistemaMedida {
+	public static void main(String[] args) {
+		String sigla = "M";
+
+		switch (sigla) {
+			case "P" -> {
+				System.out.println("PEQUENO");
+			}
+			case "M" -> {
+				System.out.println("MEDIO");
+			}
+			case "G" -> {
+				System.out.println("GRANDE");
+			}
+			default -> System.out.println("INDEFINIDO");
+		}
+	}
+}
+
+// Modo condicional if/else
+/*
+ * public class SistemaMedida {
+ * public static void main(String[] args) {
+ * String sigla = "M";
+ * 
+ * if(sigla == "P")
+ * System.out.println("PEQUENO");
+ * else if(sigla == "M")
+ * System.out.println("MEDIO");
+ * else if(sigla == "G")
+ * System.out.println("GRANDE");
+ * else
+ * System.out.println("INDEFINIDO");
+ */
