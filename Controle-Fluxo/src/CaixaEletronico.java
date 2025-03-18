1fb1895ed449efa800d4e38802e45d1b1a6b2f16
samuelherfeu <samuel75rms@gmail.com>
2025-03-18 11:27:16 -0300

    Estrutura condicional

--- Controle-Fluxo/src/CaixaEletronico.java
+++ Controle-Fluxo/src/CaixaEletronico.java
@@ -1,16 +1,15 @@
 public class CaixaEletronico {
 
-public static void main(String[] args) {
-    
-    double saldo = 25.0;
-    double valorSolicitado = 17.0;
+    public static void main(String[] args) {
 
-    if(valorSolicitado < saldo)
-     saldo = saldo - valorSolicitado;
+        double saldo = 25.0;
+        double valorSolicitado = 17.0;
 
-     System.out.println(saldo);
+        if (valorSolicitado < saldo)
+            saldo = saldo - valorSolicitado;
 
-}
+        System.out.println(saldo);
 
+    }
 
 }
